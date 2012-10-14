7c9657c8d09a82ac2678e753e08ebf717f6c632c
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-15 00:41:05 +0330

    create same menu for persian and english with php

--- Diaphoretic/menu.php
+++ Diaphoretic/menu.php
@@ -4,18 +4,56 @@
 			<div class="logo"></div>
 			<div class="header">
 				<div class="right">
-					<h1>به سایت ما خوش امدید</h1>
-					<h2>در اینجا مطلب خود را پیدا کنید</h2>
+					<h1>
+						<?php
+							if($lang == 'en') echo "welcome to our site";
+							if($lang == 'pr') echo "به سایت ما خوش امدید";
+						?>
+					</h1>
+					<h2>
+						<?php
+							if($lang == 'en') echo "find your subject here";
+							if($lang =='pr') echo "در اینجا مطلب خود را پیدا کنید";
+						?>
+					</h2>
 				</div>
 				<div class="left"></div>
 				<div class="clear"></div>
 			</div>
 			<div class="menu">
 				<ul>
-					<li><a href="./index.php">خانه</a></li>
-					<li><a href="./product.php">محصولات</a></li>
-					<li><a href="#">معرق کاری</a></li>
-					<li><a href="#">درباره ما</a></li>
+					<li>
+						<a href="./index.php">
+							<?php
+								if($lang == 'en') echo "Home";
+								if($lang == 'pr') echo "خانه";
+							?>
+						</a>
+					</li>
+					<li>
+						<a href="./product.php">
+							<?php
+								if($lang == 'en') echo "Product";
+								if($lang == 'pr') echo "محصولات";
+							?>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<?php
+								if($lang == 'en') echo "Diaphoretic";
+								if($lang == 'pr') echo "معرق کاری";
+							?>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<?php
+								if($lang == 'en') echo "About Us";
+								if($lang == 'pr') echo "درباره ما";
+							?>
+						</a>
+					</li>
 				</ul>
 			</div>
 			<div class="clear"></div>
