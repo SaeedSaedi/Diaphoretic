2157b8ba02a8b7216262c9c319d2e40a5f6711e3
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-15 21:21:40 +0330

    copy file to en and fa folder and change them to use az persian and english lang

--- Diaphoretic/index-en.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$page = 'home';
-	$lang = 'en';
-	include "header.php";
-	include "menu.php";
-	include "content-index.php";
-	include "footer.php";
-?>
--- Diaphoretic/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$page = 'home';
-	$lang = 'pr';
-	include "header.php";
-	include "menu.php";
-	include "content-index.php";
-	include "footer.php";
-?>
