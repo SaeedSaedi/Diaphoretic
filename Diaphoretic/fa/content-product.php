30bdfa95b551ad10b86d13878b1df0198b83c9e7
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-16 23:29:39 +0330

    product main design and sidebar menu

---
+++ Diaphoretic/fa/content-product.php
@@ -0,0 +1,17 @@
+<div class="container-product">
+	<div class="product">
+		<div class="sidebar">
+			<div class="sidemenu">
+				<ul>
+					<li><a href="#">گل و مرغ</a></li>
+					<li><a href="#">مینیاتور</a></li>
+					<li><a href="#">خاتون</a></li>
+					<li><a href="#">خط</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="container">
+			
+		</div>
+	</div>
+</div>
