b6f6398068de2423944d49534239fa1b260bca18
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-16 23:58:55 +0330

    product work design

--- Diaphoretic/fa/content-product.php
+++ Diaphoretic/fa/content-product.php
@@ -11,8 +11,16 @@
 			</div>
 		</div>
 		<div class="content">
-			<div class="title"><h1>گل و مرغ</h1></div>
-			<div class="work"></div>
+			<div class="title">
+				<h1>گل و مرغ</h1>
+			</div>
+			<div class="work-container">
+				<div class="work"></div>
+				<div class="work"></div>
+				<div class="work"></div>
+				<div class="work"></div>
+				<div class="clear"></div>
+			</div>
 		</div>
 		<div class="clear"></div>
 	</div>
