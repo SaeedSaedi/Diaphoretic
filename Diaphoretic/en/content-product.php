7745cfa2ddc891df640e28eedb135e066379a2e2
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-18 00:19:28 +0330

    content product en

---
+++ Diaphoretic/en/content-product.php
@@ -0,0 +1,64 @@
+<div class="container-product">
+	<div class="product">
+		<div class="sidebar">
+			<div class="sidemenu">
+				<ul>
+					<li><a href="#">گل و مرغ</a></li>
+					<li><a href="#">مینیاتور</a></li>
+					<li><a href="#">خاتون</a></li>
+					<li><a href="#">خط</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="content">
+			<div class="title">
+				<h1>گل و مرغ</h1>
+			</div>
+			<div class="work-container">
+				<div class="work">
+					<img src="" width="330" height="200" alt="" />
+					<div class="hid">
+						<p>تست تست تست تست تست</p>
+						<a href="#">بیشتر</a>
+					</div>
+				</div>
+				<div class="work">
+					<img src="" width="330" height="200" alt="" />
+					<div class="hid">
+						<p>تست تست تست تست تست</p>
+						<a href="#">بیشتر</a>
+					</div>
+				</div>
+				<div class="work">
+					<img src="" width="330" height="200" alt="" />
+					<div class="hid">
+						<p>تست تست تست تست تست</p>
+						<a href="#">بیشتر</a>
+					</div>
+				</div>
+				<div class="work">
+					<img src="" width="330" height="200" alt="" />
+					<div class="hid">
+						<p>تست تست تست تست تست</p>
+						<a href="#">بیشتر</a>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="title">
+				<h1>مینیاتور</h1>
+			</div>
+			<div class="work-container">
+				<div class="work">
+					<img src="" width="330" height="200" alt="" />
+					<div class="hid">
+						<p>تست تست تست تست تست</p>
+						<a href="#">بیشتر</a>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
