7ebe40f4f6fcf90512af3e16a16ca7744add72d2
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-18 00:46:35 +0330

    content product en

--- Diaphoretic/en/content-product.php
+++ Diaphoretic/en/content-product.php
@@ -3,57 +3,57 @@
 		<div class="sidebar">
 			<div class="sidemenu">
 				<ul>
-					<li><a href="#">گل و مرغ</a></li>
-					<li><a href="#">مینیاتور</a></li>
-					<li><a href="#">خاتون</a></li>
-					<li><a href="#">خط</a></li>
+					<li><a href="#">test test</a></li>
+					<li><a href="#">test te</a></li>
+					<li><a href="#">test t</a></li>
+					<li><a href="#">test</a></li>
 				</ul>
 			</div>
 		</div>
 		<div class="content">
 			<div class="title">
-				<h1>گل و مرغ</h1>
+				<h1>test</h1>
 			</div>
 			<div class="work-container">
 				<div class="work">
 					<img src="" width="330" height="200" alt="" />
 					<div class="hid">
-						<p>تست تست تست تست تست</p>
+						<p>test test test test</p>
+						<a href="#">More</a>
+					</div>
+				</div>
+				<div class="work">
+					<img src="" width="330" height="200" alt="" />
+					<div class="hid">
+						<p>test test test test</p>
+						<a href="#">More</a>
+					</div>
+				</div>
+				<div class="work">
+					<img src="" width="330" height="200" alt="" />
+					<div class="hid">
+						<p>test test test test</p>
 						<a href="#">بیشتر</a>
 					</div>
 				</div>
 				<div class="work">
 					<img src="" width="330" height="200" alt="" />
 					<div class="hid">
-						<p>تست تست تست تست تست</p>
-						<a href="#">بیشتر</a>
-					</div>
-				</div>
-				<div class="work">
-					<img src="" width="330" height="200" alt="" />
-					<div class="hid">
-						<p>تست تست تست تست تست</p>
-						<a href="#">بیشتر</a>
-					</div>
-				</div>
-				<div class="work">
-					<img src="" width="330" height="200" alt="" />
-					<div class="hid">
-						<p>تست تست تست تست تست</p>
-						<a href="#">بیشتر</a>
+						<p>test test test test</p>
+						<a href="#">More</a>
 					</div>
 				</div>
 				<div class="clear"></div>
 			</div>
 			<div class="title">
-				<h1>مینیاتور</h1>
+				<h1>test</h1>
 			</div>
 			<div class="work-container">
 				<div class="work">
 					<img src="" width="330" height="200" alt="" />
 					<div class="hid">
-						<p>تست تست تست تست تست</p>
-						<a href="#">بیشتر</a>
+						<p>test test test test</p>
+						<a href="#">More</a>
 					</div>
 				</div>
 				<div class="clear"></div>
