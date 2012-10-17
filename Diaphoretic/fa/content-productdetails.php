874d0bb0b7e1e77e71d706004f1528da3940085d
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-18 00:13:19 +0330

    product details design

--- Diaphoretic/fa/content-productdetails.php
+++ Diaphoretic/fa/content-productdetails.php
@@ -1,3 +1,14 @@
 <div class="container-pd">
-	<div class="pd"></div>
+	<div class="pd">
+		<div class="sidebar">
+			<div class="information"></div>
+			<div class="submit"></div>
+		</div>
+		<div class="content">
+			<div class="image"></div>
+			<div class="explain"></div>
+			<div class="clear"></div>
+		</div>
+		<div class="clear"></div>
+	</div>
 </div>
