98e3ef97dd95ff4418af914ab51a8816e0f0dd69
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-19 00:29:01 +0330

    fix bug

--- Diaphoretic/fa/content-index.php
+++ Diaphoretic/fa/content-index.php
@@ -15,7 +15,7 @@
 							<input type="text" name="firstname" class="text" placeholder="نام"><br>
 							<input type="email" name="email" class="text" placeholder="ایمیل"><br>
 							<input type="text" name="subject" class="text" placeholder="موضوع"><br>
-							<textarea rows="5" cols="10" placeholder="پیشنهادات و انتقادات خود را بیان کنید"></textarea><br />	
+							<textarea name="message" rows="5" cols="10" placeholder="پیشنهادات و انتقادات خود را بیان کنید"></textarea><br />	
 							<div class="submit">
 								<input type="image" name="submit"  src="../image/send.png" />
 							</div>
