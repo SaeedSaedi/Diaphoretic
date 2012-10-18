7feaac443d83230d29fc9516e6fe4868f746b517
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-18 16:27:33 +0330

    product details design

--- Diaphoretic/fa/content-productdetails.php
+++ Diaphoretic/fa/content-productdetails.php
@@ -1,12 +1,23 @@
 <div class="container-pd">
 	<div class="pd">
 		<div class="sidebar">
-			<div class="information"></div>
-			<div class="submit"></div>
+			<div class="information">
+				<p>نام اثر :<b> تست تست</b></p>
+				<p>طراح اثر :<b> تست تست</b></p>
+				<p>نوع اثر :<b> تست تست</b></p>
+				<p>قیمت :<b> تست تست</b></p>
+			</div>
+			<div class="submit">
+				<input type="button" value="سفارش" />
+			</div>
 		</div>
 		<div class="content">
-			<div class="image"></div>
-			<div class="explain"></div>
+			<div class="image">
+				<img src="" alt="" width="650" height="300">
+			</div>
+			<div class="explain">
+				<p>تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست تست</p>
+			</div>
 			<div class="clear"></div>
 		</div>
 		<div class="clear"></div>
