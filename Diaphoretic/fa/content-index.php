883856d919cc645e151e0b587647a4c3dfc46f26
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-19 01:13:15 +0330

    fix form

--- Diaphoretic/fa/content-index.php
+++ Diaphoretic/fa/content-index.php
@@ -11,11 +11,11 @@
 					<p>تست تست تست تست تست</p>
 				</div>
 				<div class="form">
-					<form action="../sendmail.php" method="post" target="ifrm">
-							<input type="text" name="firstname" class="text" placeholder="نام"><br>
-							<input type="email" name="email" class="text" placeholder="ایمیل"><br>
-							<input type="text" name="subject" class="text" placeholder="موضوع"><br>
-							<textarea name="message" rows="5" cols="10" placeholder="پیشنهادات و انتقادات خود را بیان کنید"></textarea><br />	
+					<form action="./sendmail.php" method="post" target="ifrm" class="contact">
+							<input type="text" id="firstname" name="firstname" class="text" placeholder="نام"><br>
+							<input type="email" id="email" name="email" class="text" placeholder="ایمیل"><br>
+							<input type="text" id="subject" name="subject" class="text" placeholder="موضوع"><br>
+							<textarea name="message" id="message"rows="5" cols="10" placeholder="پیشنهادات و انتقادات خود را بیان کنید"></textarea><br />	
 							<div class="submit">
 								<input type="image" name="submit"  src="../image/send.png" />
 							</div>
