b03ef8722af9cbcd4ac8dcf3b1b76b50d006e0d3
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-19 01:11:55 +0330

    complate mail validator

--- Diaphoretic/validator.js
+++ Diaphoretic/validator.js
@@ -7,3 +7,44 @@ function validateEmail(str){
 
 	return emailPattern.test(str);
 };
+
+$(function(){
+	$('form.contact').submit(function(){
+		var target, err = false;
+
+		target = $('input#firstname');
+		if( validateText(target.val(),3) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		};
+
+		target = $('input#subject');
+		if( validateText(target.val(),5) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		};
+
+		target = $('textarea#message');
+		if( validateText(target.val(),10) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		};
+
+		target = $('input#email');
+		if( validateEmail(target.val()) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		};
+
+		return !err;
+	});
+	
+});
