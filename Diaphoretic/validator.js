e79d7e1ca8234a2272ba91295eb8b61c3e5be972
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-19 00:52:30 +0330

    start writing validator

---
+++ Diaphoretic/validator.js
@@ -0,0 +1,9 @@
+function validateText(str,len){
+	return str.length >= len;
+};
+
+function validateEmail(str){
+	var emailPattern = /^[a-z0-9+_%.-]+@(?:[a-z0-9-]+\.)+[a-z]{2,6}$/i ;
+
+	return emailPattern.test(str);
+};
