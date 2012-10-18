eb8bd63b3bebe4e7c0a203725cc1c5830232b5dd
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-19 01:13:08 +0330

    fix form

--- Diaphoretic/en/content-index.php
+++ Diaphoretic/en/content-index.php
@@ -11,14 +11,15 @@
 					<p>test test test test test</p>
 				</div>
 				<div class="form">
-					<form action="sendmail.php" method="post">
-							<input type="text" name="firstname" class="text" placeholder="name"><br>
-							<input type="email" name="email" class="text" placeholder="email"><br>
-							<input type="text" name="subject" class="text" placeholder="subject"><br>
-							<textarea rows="5" cols="7" placeholder="Project Description"></textarea><br />	
+					<form action="./sendmail.php" method="post" target="ifrm" class="contact">
+							<input type="text" id="firstname" name="firstname" class="text" placeholder="name"><br>
+							<input type="email" id="email" name="email" class="text" placeholder="email"><br>
+							<input type="text" id="subject" name="subject" class="text" placeholder="subject"><br>
+							<textarea name="message" id="message"rows="5" cols="10" placeholder="Project Description"></textarea><br />	
 							<div class="submit">
 								<input type="image" name="submit"  src="../image/send.gif" />
 							</div>
+							<iframe id='ifrm' name='ifrm' src="" frameborder="0" scrolling="no"></iframe>
 					</form>
 				</div>
 				<div class="clear"></div>
