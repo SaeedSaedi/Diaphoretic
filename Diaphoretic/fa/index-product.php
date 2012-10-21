45104e714b298a78f0e584290ada1a01beb31ea1
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-21 15:01:09 +0330

    index with wordpress

---
+++ Diaphoretic/fa/index-product.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+		Template Name: Products
+	*/
+	$page = 'product';
+
+	get_template_part("inc/header");
+	get_template_part("inc/menu");
+	get_template_part("inc/content-product");
+	get_template_part("inc/footer");
+?>
