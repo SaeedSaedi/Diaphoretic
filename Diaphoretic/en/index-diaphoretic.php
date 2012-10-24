976acd276fb10315ed5f97f3ac33f40fbaf46aa7
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-25 01:10:31 +0330

    index diaphoretic

---
+++ Diaphoretic/en/index-diaphoretic.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+		Template Name: diaphoretic
+	*/
+	$page = 'diaphoretic';
+
+	get_template_part("inc/header");
+	get_template_part("inc/menu");
+	get_template_part("inc/content-diaphoretic");
+	get_template_part("inc/footer");
+?>
