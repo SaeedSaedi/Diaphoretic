5ca0447317be746bf7a77c6b2f60ad05b040a309
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-25 01:10:19 +0330

    index about us

---
+++ Diaphoretic/fa/index-aboutus.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+		Template Name: about us
+	*/
+	$page = 'aboutus';
+
+	get_template_part("inc/header");
+	get_template_part("inc/menu");
+	get_template_part("inc/content-aboutus");
+	get_template_part("inc/footer");
+?>
