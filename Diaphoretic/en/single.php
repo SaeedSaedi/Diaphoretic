ce966549d5c983500a5d6555bd59e24c7949157c
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-25 00:39:31 +0330

    index for product details

---
+++ Diaphoretic/en/single.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+		Template Name: product
+	*/
+	$page = 'productdetails';
+	
+	get_template_part("inc/header") ;
+	get_template_part("inc/menu");
+	get_template_part("inc/content-productdetails");
+	get_template_part("inc/footer");
+?>
