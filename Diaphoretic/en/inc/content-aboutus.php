ab28863088db33764cae13bb3b79a0f7d33be8d3
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-25 14:40:47 +0330

    about us page content

--- Diaphoretic/en/inc/content-aboutus.php
+++ Diaphoretic/en/inc/content-aboutus.php
@@ -1,3 +1,10 @@
 <div class="container-aboutus">
-	<div class="aboutus"></div>
+	<div class="content">
+		<?php get_template_part('./inc/sidebar'); ?>
+		<div class="aboutus">
+			<div class="title"><p>test test test</p></div>
+			<div class="text"><p>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test</p> </div>
+		</div>
+		<div class="clear"></div>
+	</div>
 </div>
