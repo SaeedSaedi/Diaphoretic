41408739073c9495a15fd964be9882a301607e4d
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-25 14:40:32 +0330

    load sidebar.php in index file

--- Diaphoretic/en/inc/content-index.php
+++ Diaphoretic/en/inc/content-index.php
@@ -1,29 +1,6 @@
 <div class="container-content">
 		<div class="content">
-			<div class="sidebar">
-				<div class="title">
-					<p>test test test test test</p>
-				</div>
-				<div class="txt">
-					<p>test test test test test test</p>
-				</div>
-				<div class="title">
-					<p>test test test test test</p>
-				</div>
-				<div class="form">
-					<form action="<?php bloginfo('template_url') ?>/sendmail.php" method="post" target="ifrm" class="contact">
-							<input type="text" id="firstname" name="firstname" class="text" placeholder="name"><br>
-							<input type="email" id="email" name="email" class="text" placeholder="email"><br>
-							<input type="text" id="subject" name="subject" class="text" placeholder="subject"><br>
-							<textarea name="message" id="message"rows="5" cols="10" placeholder="Project Description"></textarea><br />	
-							<div class="submit">
-								<input type="image" name="submit"  src="<?php bloginfo('template_url') ?>/image/send.gif" />
-							</div>
-							<iframe id='ifrm' name='ifrm' src="" frameborder="0" scrolling="no"></iframe>
-					</form>
-				</div>
-				<div class="clear"></div>
-			</div>
+			<?php get_template_part('./inc/sidebar'); ?>
 			<div class="container-slideshow">
 				<div class="slideshow">
 					<div class="slidebar">
