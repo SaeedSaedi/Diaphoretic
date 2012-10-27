b66adce620790d6dbcdb8af1548803aaa743cfc9
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-27 16:38:16 +0330

    content diaphoretic (first design) -en

--- Diaphoretic/en/inc/content-diaphoretic.php
+++ Diaphoretic/en/inc/content-diaphoretic.php
@@ -1,3 +1,6 @@
 <div class="container-diaphoretic">
-	<div class="diaphoretic"></div>
+	<div class="content">
+		<?php get_template_part('./inc/sidebar'); ?>
+	</div>
+	<div class="clear"></div>
 </div>
