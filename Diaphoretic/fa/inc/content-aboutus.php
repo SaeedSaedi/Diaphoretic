51a1dc451a37fdf0edf58a17360a0c6913ca2757
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-27 16:38:26 +0330

    persian txt change

--- Diaphoretic/fa/inc/content-aboutus.php
+++ Diaphoretic/fa/inc/content-aboutus.php
@@ -2,7 +2,7 @@
 	<div class="content">
 		<?php get_template_part('./inc/sidebar'); ?>
 		<div class="aboutus">
-			<div class="title"><p>تست تست تست</p></div>
+			<div class="title"><p>درباره ما</p></div>
 			<div class="text"><p>تست تست تست تست تست تست تست تست تست تست تس تست تست تست تست تست تست تست تست تست تست تست تست تس تست تست  تست تست تست تست تست تست تست تست تست تست تس تست تست</p> </div>
 		</div>
 		<div class="clear"></div>
