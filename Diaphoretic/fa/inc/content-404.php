b49fe5b65b6bdea9d349f838e5cf49eefb087cd2
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-10-29 01:59:13 +0330

    404 content design

---
+++ Diaphoretic/fa/inc/content-404.php
@@ -0,0 +1,11 @@
+<div class="container-aboutus">
+	<div class="content">
+		<?php get_template_part('./inc/sidebar'); ?>
+		<div class="aboutus">
+			<div class="title"><p>یافت نشد</p></div>
+			<div class="text"><p>صفحه مورد نظر شما موجود نمی باشد</p> 
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
